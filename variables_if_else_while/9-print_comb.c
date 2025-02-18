15aa84f1c8e6223ce6e6252a3bc0de217a9a7b2e
ShAlabbadi <10708@holbertonstudents.com>
2025-02-18 08:52:09 +0000

    Patience, persistence and perspiration make an unbeatable combination for success

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -8,12 +8,10 @@
 int main(void)
 {
 int n;
-for (n = '0'; n <= '9'; n++)
+for (n = 0; n <= 9; n++)
 {
-putchar(n);
-if (n <= 8)
-continue;
-
+putchar(n + '0');
+if (n != 9)
 putchar(',');
 putchar(' ');
 }
