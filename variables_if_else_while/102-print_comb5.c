bc09131d19333c6366062e81663585f1e185b599
ShAlabbadi <10708@holbertonstudents.com>
2025-02-18 13:58:42 +0000

    Software is eating the World

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -7,29 +7,21 @@
 */
 int main(void)
 {
-int num1;
-int num2;
-int num3;
-int num4;
-for (num1 = 0 ; num1 <= 9; num1++)
+int num1, num2;
+
+for (num1 = 0; num1 <= 98; num1++)
 {
-for (num2 = 0 ; num2 <= 8; num2++)
+for (num2 = num1 + 1; num2 <= 99; num2++)
 {
-for (num3 = 0 ; num3 <= 9; num3++)
-{
-for (num4 = 1 ; num4 <= 9; num4++)
-{
-putchar(num1 + '0');
-putchar(num2 + '0');
+putchar((num1 / 10) + '0');
+putchar((num1 % 10) + '0');
 putchar(' ');
-putchar(num3 + '0');
-putchar(num4 + '0');
-if (num1 == 9 && num2 == 8 && num3 == 9 && num4 == 9)
+putchar((num2 / 10) + '0');
+putchar((num2 % 10) + '0');
+if (num1 == 98 && num2 == 99)
 continue;
 putchar(',');
 putchar(' ');
-}
-}
 }
 }
 putchar('\n');
