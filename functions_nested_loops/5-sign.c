fbab228454060ac527d1caf318b6d16c78be0a2b
ShAlabbadi <10708@holbertonstudents.com>
2025-02-20 08:19:40 +0000

    Sign

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -4,15 +4,15 @@
 *@n : the number that the function checks.
 *Return: 1 or 0 0r -1
 */
-int print_sign(int n);
+int print_sign(int n)
 {
-if (n >= 0)
+if (n > 0)
 _putchar('+');
 return (1);
 if (n == 0)
 _putchar('0');
 return (0);
-else
+if (n < 0)
 _putchar('-');
 return (-1);
 }
