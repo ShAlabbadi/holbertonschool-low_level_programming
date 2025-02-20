910ec530c77e76bd03a0bcfd743ec2b19f55883b
ShAlabbadi <10708@holbertonstudents.com>
2025-02-20 07:28:22 +0000

    isalpha

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _isalpha - Entry point
+* @c: the variabule that the function checks
+* Return: 0 or 1
+*/
+int _isalpha(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+
+return (0);
+}
