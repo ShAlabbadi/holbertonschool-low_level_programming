f05206d5e284781f7acf082abdd2f6ae41e47441
ShAlabbadi <10708@holbertonstudents.com>
2025-02-21 22:45:38 +0000

    98 Battery Street, the OG

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* print_to_98 - function that prints all natural numbers from n to 98, followed by a new line.
+* print_to_98 - function that prints all natural numbers from n to 98.
 * @n : the number passed to the function.
 * Return : 0.
 */
