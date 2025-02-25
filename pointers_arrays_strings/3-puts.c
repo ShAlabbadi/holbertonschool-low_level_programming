c42c9b0f4b6761fab9c5363a81abffb1102ad4c1
ShAlabbadi <10708@holbertonstudents.com>
2025-02-25 16:48:41 +0000

    I do not fear computers. I fear the lack of them

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,7 +1,8 @@
 #include "main.h"
+
 /**
-* _puts - function that prints a string, followed by a new line, to stdout.
-* @str : the string.
+* _puts - Write a function that prints a string,followed by \n,to stdout.
+* @str: This is a string
 */
 void _puts(char *str)
 {
