4cb4e5073217814ed2807e571b38edec26ab2836
ShAlabbadi <10708@holbertonstudents.com>
2025-02-25 08:40:27 +0000

    Fizz-Buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -11,18 +11,24 @@ int n;
 for (n = 1; n <= 100; n++)
 {
 if ((n % 3 == 0) && (n % 5 == 0))
-printf("FizzBuzz ");
-
-else if (n % 3 == 0)
-printf("Fizz ");
-
-else if (n % 5 == 0)
-printf("Buzz ");
-
-else
-printf("%d ", n);
+{
+printf("%s", "FizzBuzz");
+putchar(' ');
 }
-
+else if (n % 3 == 0)
+{
+printf("%s", "Fizz");
+putchar(' ');
+}
+else if (n % 5 == 0)
+{
+printf("%s", "Buzz");
+putchar(' ');
+}
+else
+printf("%d", n);
+putchar(' ');
+}
 putchar('\n');
 return (0);
 }
