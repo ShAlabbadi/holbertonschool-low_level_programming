4c837fd28108ba3860ec609363a41e24abacb5f5
ShAlabbadi <10708@holbertonstudents.com>
2025-02-26 15:17:16 +0000

    strcat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -2,25 +2,17 @@
 /**
 * _strcat - function that concatenates two strings.
 * @dest: first string
-* @sec : secound string
+* @sec: secound string
 * Return: dest.
 */
 char *_strcat(char *dest, char *src)
 {
-int i, j;
+int index = 0, dest_len = 0;
 
-i = 0;
-while (dest[i++] != '\0')
-i++;
+while (dest[index++])
+dest_len++;
+for (index = 0; src[index]; index++)
+dest[dest_len++] = src[index];
 
-j = 0;
-while (src[j] != '\0')
-{
-dest[i] = src[j];
-i++;
-j++;
-}
-
-dest[i] = '\0';
 return (dest);
 }
