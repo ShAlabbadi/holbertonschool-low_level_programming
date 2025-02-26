6512e28368ea34c68868f405a65fc102f3817507
ShAlabbadi <10708@holbertonstudents.com>
2025-02-26 14:44:48 +0000

    strcat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,12 +1,16 @@
 #include "main.h"
-
+/**
+*
+*
+* Return: dest.
+*/
 char *_strcat(char *dest, char *src)
 {
-int dest_len;
+int dest_len = 0;
 int i;
 
 for (i = 0 ; src[i] != '\0'; i++)
-dest_len = i;
+dest_len++;
 
 for (i = 0 ; src[i] != '\0' ; i++)
 dest[dest_len++] = src[i];
