d3da18fdad6a37885cbeebcbbc33047f5615f53c
ShAlabbadi <10708@holbertonstudents.com>
2025-02-27 09:56:16 +0000

    Dont hate the hacker, hate the code

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -1,76 +1,36 @@
-#include <math.h> 
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <time.h> 
-  
-// Function to randomly generates password 
-// of length N 
-void randomPasswordGeneration(int N) 
-{ 
-    // Initialize counter 
-    int i = 0; 
-  
-    int randomizer = 0; 
-  
-    // Seed the random-number generator 
-    // with current time so that the 
-    // numbers will be different every time 
-    srand((unsigned int)(time(NULL))); 
-  
-    // Array of numbers 
-    char numbers[] = "0123456789"; 
-  
-    // Array of small alphabets 
-    char letter[] = "abcdefghijklmnoqprstuvwyzx"; 
-  
-    // Array of capital alphabets 
-    char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX"; 
-  
-    // Array of all the special symbols 
-    char symbols[] = "!@#$^&*?"; 
-  
-    // Stores the random password 
-    char password[N]; 
-  
-    // To select the randomizer 
-    // inside the loop 
-    randomizer = rand() % 4; 
-  
-    // Iterate over the range [0, N] 
-    for (i = 0; i < N; i++) { 
-  
-        if (randomizer == 1) { 
-            password[i] = numbers[rand() % 10]; 
-            randomizer = rand() % 4; 
-            printf("%c", password[i]); 
-        } 
-        else if (randomizer == 2) { 
-            password[i] = symbols[rand() % 8]; 
-            randomizer = rand() % 4; 
-            printf("%c", password[i]); 
-        } 
-        else if (randomizer == 3) { 
-            password[i] = LETTER[rand() % 26]; 
-            randomizer = rand() % 4; 
-            printf("%c", password[i]); 
-        } 
-        else { 
-            password[i] = letter[rand() % 26]; 
-            randomizer = rand() % 4; 
-            printf("%c", password[i]); 
-        } 
-    } 
-} 
-  
-// Driver Code 
-int main() 
-{ 
-    // Length of the password to 
-    // be generated 
-    int N = 10; 
-  
-    // Function Call 
-    randomPasswordGeneration(N); 
-  
-    return 0; 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x, sum, y;
+	int pass[100];
+
+	sum = 0;
+
+	srand(time(NULL));
+
+	for (x = 0; x < 100; x++)
+	{
+		pass[x] = rand() % 78;
+		sum += (pass[x] + '0');
+		putchar(pass[x] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			y = 2772 - sum - '0';
+			sum += y;
+			putchar(y + '0');
+			break;
+		}
+	}
+
+	return (0);
 }
