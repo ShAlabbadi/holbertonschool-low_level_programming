9da49ac4c43a631f8e9e981cb9191027b260bba6
ShAlabbadi <10708@holbertonstudents.com>
2025-03-02 10:42:29 +0000

    main.h file update

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -10,3 +10,6 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
