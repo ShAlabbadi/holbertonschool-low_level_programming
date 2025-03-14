dfd39b4a6cb62ae24ad202b8779b154d68250064
ShAlabbadi <10708@holbertonstudents.com>
2025-03-14 23:25:37 +0000

    Trust no one

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -3,7 +3,7 @@
 /**
 * malloc_checked - a function that allocates memory using malloc.
 * @b : the address.
-* Returns: a pointer to the allocated memory.
+* Return: a pointer to the allocated memory.
 */
 void *malloc_checked(unsigned int b)
 {
