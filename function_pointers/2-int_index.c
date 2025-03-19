93cb52101452be7e936c61e75e03b0e7323fe81c
ShAlabbadi <10708@holbertonstudents.com>
2025-03-19 18:21:55 +0000

    2. To hell with circumstances; I create opportunities

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -15,10 +15,10 @@ int i;
 if (array == NULL || size <= 0 || cmp == NULL)
 		return (-1);
 
-for (i = 0; i < size ; i ++)
+for (i = 0; i < size ; i++)
 {
 	if(cmp(array[i]))
-	return (1);
+	return (i);
 }
 
 return (-1);
