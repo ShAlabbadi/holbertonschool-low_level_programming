0c2f023249c08c0510521169725729bd00761831
ShAlabbadi <10708@holbertonstudents.com>
2025-03-21 23:39:59 +0000

    4. Real programmers can write assembly code in any language

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,16 +1,17 @@
-section .data
-        message db "Hello, World!",10
+section .data:
+	message db "Hello, World", 10
 
-section .text
-        global _start
+section .text:
+	global main
 
-_start:
-        mov rax, 1
-        mov rdi, 1
-        mov rsi, message
-        mov rdx, 14
-        syscall
+main:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, message
+	mov rdx, 13
+	syscall
 
-        mov rax, 60
-        mov rdi, 0
-        syscall
+
+	mov rax, 60
+	mov rdi, 0
+	syscall	
